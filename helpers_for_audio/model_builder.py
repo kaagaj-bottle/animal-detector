a53855dfe70a3d5f45c3424f2be9a4ed9eee0c65
Zishan Siddique <zishansiddique80@gmail.com>
2024-03-03 10:38:50 +0545

    added model builder and utils to print summary

---
+++ helpers_for_audio/model_builder.py
@@ -0,0 +1,27 @@
+import torch
+import torchvision
+from torchvision.models import efficientnet_b0
+
+# check the device
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'Device: {device}')
+
+
+
+def construct_model(output_shape:int):
+    model=efficientnet_b0(weights="DEFAULT").to(device)
+
+    for param in model.features.parameters():
+        param.requires_grad=False    
+    
+    model.classifier=torch.nn.Sequential(
+        torch.nn.Dropout(p=0.2,inplace=True),
+        torch.nn.Linear(in_features=1280,
+                        out_features=output_shape,
+                        bias=True).to(device)
+    )
+    return model
+
+
+
+
